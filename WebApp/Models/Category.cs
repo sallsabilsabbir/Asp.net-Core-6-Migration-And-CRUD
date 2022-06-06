84e38166b1305a83d34dd45086bace405bc5a877
scube <scube@SCUBE>
2022-06-06 17:09:21 +0600

    Add project files.

---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 500, ErrorMessage = "Display Order must be between 1 and 500 only!!")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
